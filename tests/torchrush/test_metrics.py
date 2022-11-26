7c2d2a703a9c47bee1b0aab37070073fc81e5924
fcakyon <fcakyon@gmail.com>
2022-11-26 09:47:08 +0300

    refactor tests

--- tests/torchrush/test_metrics.py
+++ tests/torchrush/test_metrics.py
@@ -27,12 +27,14 @@ class CallbackForTestingTrainer(Callback):
             self.losses.append(loss.item())
 
 
-def get_rush_model():
+@pytest.fixture
+def rush_model():
     pl.seed_everything(42)
     return LeNetForClassification(optimizer="SGD", criterion="CrossEntropyLoss", input_size=(28, 28, 1), lr=0.01)
 
 
-def get_data_loaders():
+@pytest.fixture
+def data_loaders():
     train_loader = DataLoader.from_datasets(
         "mnist", split="train", constructor=GenericImageClassificationDataset, batch_size=32
     )
@@ -42,21 +44,52 @@ def get_data_loaders():
     return train_loader, val_loader
 
 
-def test_rushmetrics():
+@pytest.fixture
+def metric_callback():
     metric_callback = MetricCallback(metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end")
     assert len(metric_callback.combined_evaluations["train"].metrics) == 4
     assert len(metric_callback.combined_evaluations["val"].metrics) == 4
     assert len(metric_callback.combined_evaluations["val"].metrics) == 4
+    return metric_callback
 
+
+@pytest.fixture
+def combined_evaluations():
     # check if metrics are correctly added to the combined evaluations
-    combined_evaluations = CombinedEvaluations(metrics=["accuracy"])
+    combined_evaluations = CombinedEvaluations(metrics=["accuracy", "f1", "precision", "recall"])
     mock_metric = MagicMock()
     combined_evaluations.metrics = [mock_metric]
     combined_evaluations.add_batch([0, 0, 0], [0, 1, 0])
-
     mock_metric.add_batch.assert_called_with(predictions=[0, 0, 0], references=[0, 1, 0])
+    return combined_evaluations
 
-    # check if metrics are correctly computed in the combined evaluations
+
+@pytest.fixture
+def mock_metric():
+    mock_metric = MagicMock()
+    return mock_metric
+
+
+@pytest.fixture
+def mock_logger():
+    mock_logger = MagicMock()
+    return mock_logger
+
+
+@pytest.fixture
+def batch_step_outputs():
+    outputs = {
+        "loss": torch.tensor(0.5),
+        "predictions": torch.tensor([1, 0, 1, 1]),
+        "references": torch.tensor([1, 1, 1, 0]),
+    }
+    return outputs
+
+
+def test_rushmetrics_accuracy(combined_evaluations, mock_metric):
+    mock_metric = MagicMock()
+
+    # check if accuracy is correctly computed in the combined evaluations
     mock_metric.name = "accuracy"
     combined_evaluations.metrics = [mock_metric]
     combined_evaluations.compute()
@@ -64,6 +97,9 @@ def test_rushmetrics():
     combined_evaluations.compute(labelwise=True)
     mock_metric.compute.assert_called_with()
 
+
+def test_rushmetrics_precision(combined_evaluations, mock_metric):
+    # check if precision is correctly computed in the combined evaluations
     mock_metric.name = "precision"
     combined_evaluations.metrics = [mock_metric]
     combined_evaluations.compute()
@@ -71,6 +107,11 @@ def test_rushmetrics():
     combined_evaluations.compute(labelwise=True)
     mock_metric.compute.assert_called_with(average=None, zero_division=0)
 
+
+def test_rushmetrics_recall(combined_evaluations, mock_metric):
+    mock_metric = MagicMock()
+
+    # check if recall is correctly computed in the combined evaluations
     mock_metric.name = "recall"
     combined_evaluations.metrics = [mock_metric]
     combined_evaluations.compute()
@@ -79,36 +120,29 @@ def test_rushmetrics():
     mock_metric.compute.assert_called_with(average=None)
 
 
-def test_rushmetrics_callback():
-    tb_logger = MagicMock()
-    rush_model = get_rush_model()
-
-    outputs = {
-        "loss": torch.tensor(0.5),
-        "predictions": torch.tensor([1, 0, 1, 1]),
-        "references": torch.tensor([1, 1, 1, 0]),
-    }
-
+def test_rushmetrics_callback_train_batch_end(rush_model, mock_logger, batch_step_outputs):
     # scenario 1: batch_end + train
     metric_callback = MetricCallback(
         metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end", log_labelwise_metrics=False
     )
     metric_callback.combined_evaluations = MagicMock()
 
-    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[tb_logger], callbacks=[metric_callback])
-    assert pl_trainer.loggers == [tb_logger]
+    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[mock_logger], callbacks=[metric_callback])
+    assert pl_trainer.loggers == [mock_logger]
     rush_model.trainer = pl_trainer
-    assert rush_model.loggers == [tb_logger]
+    assert rush_model.loggers == [mock_logger]
 
     batch_idx = 33
     assert batch_idx % metric_callback.eval_freq == 0
-    metric_callback.on_train_batch_end(pl_trainer, rush_model, outputs, None, batch_idx)
+    metric_callback.on_train_batch_end(pl_trainer, rush_model, batch_step_outputs, None, batch_idx)
     metric_callback.combined_evaluations["train"].add_batch.assert_called_with(
-        predictions=outputs["predictions"], references=outputs["references"]
+        predictions=batch_step_outputs["predictions"], references=batch_step_outputs["references"]
     )
     metric_callback.combined_evaluations["train"].compute.assert_called_with(labelwise=False)
-    tb_logger.log_any.assert_called_with({"train/loss": outputs["loss"].item()}, batch_idx)
+    mock_logger.log_any.assert_called_with({"train/loss": batch_step_outputs["loss"].item()}, batch_idx)
 
+
+def test_rushmetrics_callback_val_batch_end(rush_model, mock_logger, batch_step_outputs):
     # scenario 2: batch_end + val
     metric_callback = MetricCallback(
         metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end", log_labelwise_metrics=False
@@ -117,18 +151,20 @@ def test_rushmetrics_callback():
     metric_callback.combined_evaluations["val"].metrics[0].name = "accuracy"
     metric_callback.combined_evaluations["val"].metrics[1].name = "f1"
 
-    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[tb_logger], callbacks=[metric_callback])
-    assert pl_trainer.loggers == [tb_logger]
+    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[mock_logger], callbacks=[metric_callback])
+    assert pl_trainer.loggers == [mock_logger]
     rush_model.trainer = pl_trainer
-    assert rush_model.loggers == [tb_logger]
+    assert rush_model.loggers == [mock_logger]
 
     batch_idx = 35
     assert batch_idx % metric_callback.eval_freq == 0
-    metric_callback.on_validation_batch_end(pl_trainer, rush_model, outputs, None, batch_idx, 0)
+    metric_callback.on_validation_batch_end(pl_trainer, rush_model, batch_step_outputs, None, batch_idx, 0)
     metric_callback.combined_evaluations["val"].metrics[0].compute.assert_called_with()
     metric_callback.combined_evaluations["val"].metrics[1].compute.assert_called_with(average="macro")
-    tb_logger.log_any.assert_called_with({"val/loss": outputs["loss"].item()}, batch_idx)
+    mock_logger.log_any.assert_called_with({"val/loss": batch_step_outputs["loss"].item()}, batch_idx)
 
+
+def test_rushmetrics_callback_test_batch_end_labelwise(rush_model, mock_logger, batch_step_outputs):
     # scenario 3: labelwise + batch_end + test
     metric_callback = MetricCallback(
         metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end", log_labelwise_metrics=True, labels=[0, 1]
@@ -137,49 +173,57 @@ def test_rushmetrics_callback():
     metric_callback.combined_evaluations["test"].metrics[0].name = "accuracy"
     metric_callback.combined_evaluations["test"].metrics[1].name = "f1"
 
-    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[tb_logger], callbacks=[metric_callback])
-    assert pl_trainer.loggers == [tb_logger]
+    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[mock_logger], callbacks=[metric_callback])
+    assert pl_trainer.loggers == [mock_logger]
     rush_model.trainer = pl_trainer
-    assert rush_model.loggers == [tb_logger]
+    assert rush_model.loggers == [mock_logger]
 
     batch_idx = 37
     assert batch_idx % metric_callback.eval_freq == 0
-    metric_callback.on_test_batch_end(pl_trainer, rush_model, outputs, None, batch_idx, 0)
+    metric_callback.on_test_batch_end(pl_trainer, rush_model, batch_step_outputs, None, batch_idx, 0)
     metric_callback.combined_evaluations["test"].metrics[0].compute.assert_called_with()
     metric_callback.combined_evaluations["test"].metrics[1].compute.assert_called_with(average=None)
-    tb_logger.log_any.assert_called_with({"test/loss": outputs["loss"].item()}, batch_idx)
+    mock_logger.log_any.assert_called_with({"test/loss": batch_step_outputs["loss"].item()}, batch_idx)
 
+
+def test_rushmetrics_callback_val_epoch_end_labelwise(rush_model, mock_logger, batch_step_outputs):
     # scenario 4: labelwise + epoch_end + validation
     metric_callback = MetricCallback(
         metrics=["accuracy", "f1", "precision", "recall"], log_on="epoch_end", log_labelwise_metrics=True, labels=[0, 1]
     )
 
-    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[tb_logger], callbacks=[metric_callback])
-    assert pl_trainer.loggers == [tb_logger]
+    pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=[mock_logger], callbacks=[metric_callback])
+    assert pl_trainer.loggers == [mock_logger]
     pl_trainer.fit_loop.epoch_progress.current.completed = 1  # set current_epoch to 1
     assert pl_trainer.current_epoch == 1
     assert pl_trainer.current_epoch % metric_callback.eval_freq == 0
     rush_model.trainer = pl_trainer
-    assert rush_model.loggers == [tb_logger]
+    assert rush_model.loggers == [mock_logger]
 
     batch_idx = 39
-    metric_callback.on_validation_batch_end(pl_trainer, rush_model, outputs, None, batch_idx, 0)
+    metric_callback.on_validation_batch_end(pl_trainer, rush_model, batch_step_outputs, None, batch_idx, 0)
     metric_callback.on_validation_epoch_end(pl_trainer, rush_model)
-    tb_logger.log_any.assert_called_with({"val/recall_1": 0.6666666666666666}, 1)
+    mock_logger.log_any.assert_called_with({"val/recall_1": 0.6666666666666666}, 1)
 
 
-def test_pltrainer_trains_with_rushmetrics():
-    # exp 1
-    metric_callback = MetricCallback(metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end")
-
+def test_pltrainer_trains_with_rushmetrics(rush_model, data_loaders):
     tb_logger = TensorBoardLogger(save_dir=TEMP_LOG_DIR)
 
-    train_loader, val_loader = get_data_loaders()
-    rush_model = get_rush_model()
+    metric_callback = MetricCallback(metrics=["accuracy", "f1", "precision", "recall"], log_on="batch_end")
+
+    train_loader, val_loader = data_loaders
     pl_trainer = pl.Trainer(enable_checkpointing=False, max_steps=20, logger=tb_logger, callbacks=[metric_callback])
     pl_trainer.fit(rush_model, train_loader, val_loader)
 
-    # exp 2
+    # remove the temp dir
+    shutil.rmtree(TEMP_LOG_DIR)
+
+
+def test_pltrainer_trains_with_rushmetrics_labelwise(rush_model, data_loaders):
+    tb_logger = TensorBoardLogger(save_dir=TEMP_LOG_DIR)
+
+    train_loader, val_loader = data_loaders
+
     metric_callback = MetricCallback(
         metrics=["accuracy", "f1", "precision", "recall"],
         log_on="batch_end",
